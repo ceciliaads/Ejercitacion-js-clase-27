45801c746165928150cd19822225ed6f27825023
Cecilia Da Silva <ceciliaadasilva@hotmail.com>
2021-04-10 13:07:18 -0300

    first commit. Ejercicios resueltos hasta el punto 4 inclusive

---
+++ index.js
@@ -0,0 +1,33 @@
+// EJERCICIO 1
+ // function tienenMismaLongitud (a, b) {
+ //     return(a.length === b.length) ;
+ // }
+
+ // tienenMismaLongitud('javascript', 'java');
+ // tienenMismaLongitud('manzana', 'cerveza');
+
+// EJERCICIO 2
+ // function esElUltimoCaracter (palabra, caracter) {
+ //     const ultimaLetra = (palabra.slice(-1))
+ //   return (palabra.includes(caracter, ultimaLetra));
+ // }
+
+ // esElUltimoCaracter('lovelace', 'e');
+ // esElUltimoCaracter('lovelace', 'f');
+
+// EJERCICIO 3
+ // function esValida (contrasenia) {
+ //     return contrasenia.length >= 8
+ //    }
+   
+ //    esValida('contraseniaMuySegura');
+ //    esValida('abc123');
+
+// EJERCICIO 4
+ // function sonIguales (a, b) {
+ //     console.log(a.toLowerCase() === b.toLowerCase());
+ // }
+
+ // sonIguales('javascript', 'JavaScript');
+ // sonIguales('AdA LoVeLaCe', 'Ada Lovelace');
+ // sonIguales('NO ESTOY GRITANDO', 'ESTOY GRITANDO');
